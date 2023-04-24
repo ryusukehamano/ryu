7a8078200b3eab617f6d53d728dc1e74261be44e
ryusukehamano <ryu20127@gmail.com>
2023-04-24 17:12:27 +0900

    good

--- sample.cpp
+++ sample.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
 
+int sum(int, int);
+
 int main()
 {
     int a = 5;
     int b = 3;
-    std::cout << a + b << std::endl;
+    std::cout << sum(a, b) << std::endl;
     return 0;
+}
+
+int sum(int a, int b)
+{
+    return a + b;
 }
